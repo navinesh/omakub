bad25e5f0fc8f6ab19a996a7972f41525a8c975f
Navinesh Chand <navinesh@hey.com>
2024-06-13 16:14:51 +1200

    Update fonts.sh

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,15 +1,10 @@
 mkdir -p ~/.local/share/fonts
 
-cd /tmp
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
-unzip CascadiaMono.zip -d CascadiaFont
-cp CascadiaFont/*.ttf ~/.local/share/fonts
-rm -rf CascadiaMono.zip CascadiaFont
-
-wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
-unzip iafonts.zip -d iaFonts
-cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
-rm -rf iafonts.zip iaFonts
+cd ~/Downloads
+wget https://github.com/microsoft/cascadia-code/releases/download/v2404.23/CascadiaCode-2404.23.zip
+unzip CascadiaCode-2404.23.zip -d CascadiaFont
+cp CascadiaFont/ttf/*.ttf ~/.local/share/fonts
+rm -rf CascadiaCode-2404.23.zip CascadiaFont
 
 fc-cache
 cd -
